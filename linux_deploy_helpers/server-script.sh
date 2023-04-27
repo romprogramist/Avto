4b44d4e6ca7e75226ec7486b55860aae7b075ec3
zolotoy <romprogramist@gmail.com>
2023-04-28 03:33:11 +0400

    ok

--- linux_deploy_helpers/server-script.sh
+++ linux_deploy_helpers/server-script.sh
@@ -2,7 +2,7 @@
 
 # rename all <your_project_name> entries and put this file on server
   
-sudo service your_project_name stop
-sudo unzip -o your_project_name.zip -d /var/www/your_project_name # your project folder on server
-sudo rm your_project_name.zip
-sudo service your_project_name start
+sudo service avto stop
+sudo unzip -o avto.zip -d /var/www/avto # your project folder on server
+sudo rm avto.zip
+sudo service avto start
